05577c314f1d2f2541ae0fcb16d0200e33bf024e
Ahmed Gobran <115479528+Ahmedgobran@users.noreply.github.com>
2022-10-24 21:26:39 +0300

    Add files via upload

---
+++ name comands using title(), upper() and lower().py
@@ -0,0 +1,4 @@
+name="ahmed gobran"
+print(name.title())  #.title()makes first letter of each word in string capital
+print(name.lower())  #.lower()changes whole string to lowecase (all the letters become lower)
+print(name.upper())  #.upper()changes whole string to uppercase (all the letters become upper)
