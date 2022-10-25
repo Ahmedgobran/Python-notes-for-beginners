c68fe03107001e072c0d06c82b8b264bcd741004
Ahmed Gobran <115479528+Ahmedgobran@users.noreply.github.com>
2022-10-25 22:39:16 +0300

    Add files via upload

---
+++ max() and min() functions.py
@@ -0,0 +1,14 @@
+text="Hello world"
+print(max(text))
+numbers= 1,2,3
+print(max(numbers))      # max() function prints the letter closest to Z if string,  and highest value if integer
+
+
+text="helloworld"
+print(min(text)) # min() function prints the letter closest to A (unless the word starts with a capital letter so it prints it in this case) 
+
+text="hello world"  
+print(min(text))               # if there is a space in the string it prints it as lowest value bcoz theres nothing
+
+numbers=1,2,3  
+print(min(numbers))  # min() function prints lowest value in this case if integer
